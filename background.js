b721b168cc666b56802c2ccd3fc3eaef51f459d7
ruan lianjie <ruan.lj@foxmail.com>
2016-11-28 11:06:37 +0800

    代码风格修改

--- background.js
+++ background.js
@@ -1,78 +1,79 @@
 /*
-ÍøÖ·£ºhttps://github.com/ruanima/BaiGoogleDu
-¹¦ÄÜ£º¹È¸è°Ù¶ÈÒ»¼üËÑË÷ÐÞ¸Ä°æ
-´´½¨£º2015.11.25
-×÷Õß£ºponder.work
-·´À¡£º190127701@qq.com
+ç½‘å€ï¼šhttps://github.com/ruanima/BaiGoogleDu
+åŠŸèƒ½ï¼šè°·æ­Œç™¾åº¦ä¸€é”®æœç´¢ä¿®æ”¹ç‰ˆ
+åˆ›å»ºï¼š2015.11.25
+ä½œè€…ï¼šponder.work
+åé¦ˆï¼š190127701@qq.com
 forked from: raywill/BaiGoogleDu
 */
+
 var isOpen = true ,
-	DELAY_POPICON = 2000,
-	DELAY_KILLEDICON = 3000;
+    DELAY_POPICON = 2000,
+    DELAY_KILLEDICON = 3000;
 
 chrome.extension.onRequest.addListener(
-	function(request, sender, sendResponse) {
-		switch( request.action ){
-			case 'getIsOpen':
-				sendResponse({isOpen: isOpen});
-				break;
-			case 'setKilledNum':
-				if( request.killedNum > 0 ){
-					setBadge( request.killedNum ,  [255 , 0, 0, 100] );
-					removeBadge( DELAY_KILLEDICON );
-				}
-				break;
-			case 'setKeywordLog':
-				//alert(request.keyword + request.flag);
-				//var url = "http://1.mydream.sinaapp.com/kw.php?kw=" +encodeURIComponent(request.keyword) + "&f=" + request.flag;
-				//$.getJSON(url);
-				break;
-			default:break;
-		}
-	}
+    function(request, sender, sendResponse) {
+        switch( request.action ){
+            case 'getIsOpen':
+                sendResponse({isOpen: isOpen});
+                break;
+            case 'setKilledNum':
+                if( request.killedNum > 0 ){
+                    setBadge( request.killedNum ,  [255 , 0, 0, 100] );
+                    removeBadge( DELAY_KILLEDICON );
+                }
+                break;
+            case 'setKeywordLog':
+                //alert(request.keyword + request.flag);
+                //var url = "http://1.mydream.sinaapp.com/kw.php?kw=" +encodeURIComponent(request.keyword) + "&f=" + request.flag;
+                //$.getJSON(url);
+                break;
+            default:break;
+        }
+    }
 );
 
 chrome.browserAction.onClicked.addListener(function(tabs) {
-	switchIcon();
-	isOpen = !isOpen ;
-	isOpen ? console.log('OPEN -' + getTime()) : console.log('CLOSE-' + getTime());
-	chrome.tabs.executeScript(null, {file:"killer.js"});
+    switchIcon();
+    isOpen = !isOpen ;
+    isOpen ? console.log('OPEN -' + getTime()) : console.log('CLOSE-' + getTime());
+    chrome.tabs.executeScript(null, {file:"killer.js"});
 });
 
 function getTime(){
-	var str = '',
-		d = new Date(),
-		h = d.getHours(),
-		m = d.getMinutes(),
-		s = d.getSeconds();
-	str = h + ':' + ( m < 10 ? '0' + m : m ) + ':' + ( s < 10 ? '0' + s : s );
-	return str;
+    var str = '',
+        d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes(),
+        s = d.getSeconds();
+    str = h + ':' + ( m < 10 ? '0' + m : m ) + ':' + ( s < 10 ? '0' + s : s );
+    return str;
 }
 
 function setBadge( text , color ){
-	chrome.browserAction.setBadgeText({"text": text || ''});
-	color && chrome.browserAction.setBadgeBackgroundColor({"color": color });
+    chrome.browserAction.setBadgeText({"text": text || ''});
+    color && chrome.browserAction.setBadgeBackgroundColor({"color": color });
 }
 
 function removeBadge( delay ){
-	//ÒÆ³ý±ê¼Ç
-	clearTimeout( window.t );
-	window.t = setTimeout(function(){
-		setBadge();
-		clearTimeout( window.t );
-	}, delay );
+    //ç§»é™¤æ ‡è®°
+    clearTimeout( window.t );
+    window.t = setTimeout(function(){
+        setBadge();
+        clearTimeout( window.t );
+    }, delay );
 }
 
 function switchIcon(){
-	var icon = "logo48.png",
-		badgeText = 'ON',
-		color = [255, 0, 0, 50];
-	if( isOpen ){//sign allowed
-		icon = "logo48_.png";
-		badgeText = 'OFF';
-		color = [0, 255, 0, 50];
-	}
-	chrome.browserAction.setIcon({path:icon});
-	setBadge( badgeText, color );
-	removeBadge( DELAY_POPICON );
+    var icon = "logo48.png",
+        badgeText = 'ON',
+        color = [255, 0, 0, 50];
+    if( isOpen ){//sign allowed
+        icon = "logo48_.png";
+        badgeText = 'OFF';
+        color = [0, 255, 0, 50];
+    }
+    chrome.browserAction.setIcon({path:icon});
+    setBadge( badgeText, color );
+    removeBadge( DELAY_POPICON );
 }
--- killer.js
+++ killer.js
@@ -10,119 +10,112 @@ forked from: raywill/BaiGoogleDu
 var isOpen = true;
 
 chrome.extension.sendRequest({action: "getIsOpen"}, function(response) {
-	isOpen = response.isOpen ;
-	//alert("Hi tehre");
-	if( isOpen ){
-		googleBaiduer();
-	}
+    isOpen = response.isOpen ;
+    //alert("Hi tehre");
+    if( isOpen ){
+        googleBaiduer();
+    }
 });
 
-function insertAfter(newElement, targetElement)
- {
-           var parentEl = targetElement.parentNode;
-            if(parentEl.lastChild == targetElement)
-            {
-                parentEl.appendChild(newElement);
-            }else
-            {
-                parentEl.insertBefore(newElement,targetElement.nextSibling);
-            }            
+function insertAfter(newElement, targetElement) {
+    var parentEl = targetElement.parentNode;
+    if(parentEl.lastChild == targetElement) {
+        parentEl.appendChild(newElement);
+    } else {
+        parentEl.insertBefore(newElement,targetElement.nextSibling);
+    }
 };
 
 function googleBaiduer(){
-	//日志输出
-	//console.log('【Google Baidu搜索穿越】日志输出');
-	// console.log('执行时刻：' + getTime() );
-	// console.log('\n');
+    //日志输出
+    //console.log('【Google Baidu搜索穿越】日志输出');
+    // console.log('执行时刻：' + getTime() );
+    // console.log('\n');
+    //alert("Hi tehre");
+    //var s = document.getElementsByClassName('d_sign_split');
+    var s1 =  document.getElementsByName("btnK");
+    //var s2 =  document.getElementsByName("q") ;
+    var s2 = document.getElementById("sblsbb");
+    //var s2 = document.getElementById("tsf");
 
+    var b1 =  document.getElementById("su");
+    var result = document.getElementById('wrapper_wrapper');
 
+    if (b1 && result.children.length > 0 ) {
+        // Search Google in Baidu
+        var BaiduBtn = document.createElement("input");
+        BaiduBtn.type = "submit";
+        BaiduBtn.value = "Google";
+        BaiduBtn.className = "s_btn btn";
+        BaiduBtn.style.marginLeft = "1px";
+        BaiduBtn.onclick = function(){
+            //alert("clicked");
+            var keyword_input = document.getElementById("kw");
+            //alert(keyword_input.value);
+            setKeywordLog(keyword_input.value, "G");
+            window.location.href="https://www.google.com.hk/search?q="+keyword_input.value;
+            return false;
+        };
+        insertAfter(BaiduBtn, b1);
 
-	//alert("Hi tehre");
-	//var s = document.getElementsByClassName('d_sign_split');
-	var s1 =  document.getElementsByName("btnK");
-	//var s2 =  document.getElementsByName("q") ;
-	var s2 = document.getElementById("sblsbb");
-	//var s2 = document.getElementById("tsf");
-	
-	var b1 =  document.getElementById("su");
-	var result = document.getElementById('wrapper_wrapper');
+    } else if(s1 || s2) {
+        var BaiduBtn = document.createElement("input");
+        BaiduBtn.type = "submit";
+        BaiduBtn.value = "Baidu Search";
+        BaiduBtn.name = "btnK";
+        BaiduBtn.onclick = function(){
+            //alert("clicked");
+            var keyword_input = document.getElementsByName("q");
+            //alert(keyword_input[0].value);
+            setKeywordLog(keyword_input[0].value, "B");
+            window.location.href='http://www.baidu.com/s?wd=' + keyword_input[0].value;
+            return false;
+        };
+        insertAfter(BaiduBtn, s1[0]);
 
-	if (b1 && result.children.length > 0 )  // Search Google in Baidu
-	{
-		var BaiduBtn = document.createElement("input");
-		BaiduBtn.type = "submit";
-		BaiduBtn.value = "Google";
-		BaiduBtn.className = "s_btn btn";
-		BaiduBtn.style.marginLeft = "1px";
-		BaiduBtn.onclick = function(){
-			//alert("clicked");
-			var keyword_input = document.getElementById("kw");
-			//alert(keyword_input.value);
-			setKeywordLog(keyword_input.value, "G");
-			window.location.href="https://www.google.com.hk/search?q="+keyword_input.value;
-			return false;
-		};
-		insertAfter(BaiduBtn, b1);
-	}
-	else if(s1 || s2)
-	{
-		var BaiduBtn = document.createElement("input");
-		BaiduBtn.type = "submit";
-		BaiduBtn.value = "Baidu Search";
-		BaiduBtn.name = "btnK";
-		BaiduBtn.onclick = function(){
-			//alert("clicked");
-			var keyword_input = document.getElementsByName("q");
-			//alert(keyword_input[0].value);
-			setKeywordLog(keyword_input[0].value, "B");
-			window.location.href='http://www.baidu.com/s?wd=' + keyword_input[0].value;
-			return false;
-		};
-		insertAfter(BaiduBtn, s1[0]);
-
-		var BaiduBtn2 = document.createElement("input");
-		BaiduBtn2.type = "submit";
-		BaiduBtn2.value = "du";
-		BaiduBtn2.style.position = "absolute";
-		BaiduBtn2.style.border = "none";
-		BaiduBtn2.style.fontSize = '2em';
-		BaiduBtn2.style.fontWeight = 'bold';
-		BaiduBtn2.style.color = 'white';
-		BaiduBtn2.style.margin = "0 0 0 1px";
-		BaiduBtn2.style.padding = "0";
-		BaiduBtn2.style.width = "40px";
-		BaiduBtn2.style.height = "40px";
-		BaiduBtn2.style.borderRadius = "3px"; 
-		BaiduBtn2.style.background = "#4487F7";
-		BaiduBtn2.onclick = function(){
-			//alert("clicked");
-			var keyword_input = document.getElementsByName("q");
-			//alert(keyword_input[0].value);
-			setKeywordLog(keyword_input[0].value, "B");
-			window.location.href='http://www.baidu.com/s?wd=' + keyword_input[0].value;
-			return false;
-		};
-		s2.appendChild(BaiduBtn2);
-	}
+        var BaiduBtn2 = document.createElement("input");
+        BaiduBtn2.type = "submit";
+        BaiduBtn2.value = "du";
+        BaiduBtn2.style.position = "absolute";
+        BaiduBtn2.style.border = "none";
+        BaiduBtn2.style.fontSize = '2em';
+        BaiduBtn2.style.fontWeight = 'bold';
+        BaiduBtn2.style.color = 'white';
+        BaiduBtn2.style.margin = "0 0 0 1px";
+        BaiduBtn2.style.padding = "0";
+        BaiduBtn2.style.width = "40px";
+        BaiduBtn2.style.height = "40px";
+        BaiduBtn2.style.borderRadius = "3px";
+        BaiduBtn2.style.background = "#4487F7";
+        BaiduBtn2.onclick = function(){
+            //alert("clicked");
+            var keyword_input = document.getElementsByName("q");
+            //alert(keyword_input[0].value);
+            setKeywordLog(keyword_input[0].value, "B");
+            window.location.href='http://www.baidu.com/s?wd=' + keyword_input[0].value;
+            return false;
+        };
+        s2.appendChild(BaiduBtn2);
+    }
 }
 
 function getTime(){
-	var str = '',
-		d = new Date(),
-		h = d.getHours(),
-		m = d.getMinutes(),
-		s = d.getSeconds();
-	str = h + ':' + ( m < 10 ? '0' + m : m ) + ':' + ( s < 10 ? '0' + s : s );
-	return str;
+    var str = '',
+        d = new Date(),
+        h = d.getHours(),
+        m = d.getMinutes(),
+        s = d.getSeconds();
+    str = h + ':' + ( m < 10 ? '0' + m : m ) + ':' + ( s < 10 ? '0' + s : s );
+    return str;
 }
 
 
 function setKeywordLog( kw, flag ){
-	chrome.extension.sendRequest({action: "setKeywordLog", keyword: kw, flag: flag }, function(response) {
-	});
+    chrome.extension.sendRequest({action: "setKeywordLog", keyword: kw, flag: flag }, function(response) {
+    });
 };
 
 function setKilledNum( n ){
-	chrome.extension.sendRequest({action: "setKilledNum",killedNum: n+'' }, function(response) {
-	});
+    chrome.extension.sendRequest({action: "setKilledNum",killedNum: n+'' }, function(response) {
+    });
 };
